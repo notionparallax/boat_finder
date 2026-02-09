54e6bb6638d89e740547ae6817ba697cb1729c7f
Ben Doherty <ben@notionparallax.co.uk>
2026-02-09 16:15:24 +1100

    Add Azure Static Web Apps deployment workflow

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 // Entry point for Azure Functions - imports all function definitions
-import './functions/users.js';
-import './functions/availability.js';
-import './functions/sites.js';
 import './functions/admin.js';
+import './functions/availability.js';
 import './functions/daily-digest.js';
+import './functions/sites.js';
+import './functions/users.js';
+
